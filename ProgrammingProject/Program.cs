1d9106397a66c53823603b3b92e0a79f792a614b
Daniel Dixon <14dixond@utcsheffield.org.uk>
2018-11-14 12:35:21 +0000

    Add project files.

---
+++ ProgrammingProject/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingProject{
+
+    class Player{
+        public string name;
+        public int round;
+        public int winrate;
+        public Player()
+        {
+           
+        }
+
+    }
+
+    class Program{
+
+        static void Main(string[] args){
+
+        }
+    }
+}
